c26f895b1d41c508c57fe6f52899dc3435736ae0
gololobov <gololobov.serhiy@gmail.com>
2024-09-18 20:41:33 +0300

    18.09.2024

---
+++ Lesson 1.py
@@ -0,0 +1,36 @@
+'''print("Hello Python", end=' ')
+print("Мене звати Сергій")
+print(123, 45, "Hello", sep=' + ')
+print("\nHello")
+print("\tHello\t\t\tPython")
+print("Hello\rPython")
+print("Hello\bPython")'''
+
+#print(123)
+#print(123.32)
+
+# a-z, A-Z, 0-9, _
+# _1a
+
+# a = 10
+# print(a)
+
+# int
+# float
+# str
+# bool
+
+# a = int(input("Введитиь а: "))
+# b = int(input("Введитиь b: "))
+# print(a + b)
+
+
+
+
+a = int(input("Введитиь а: "))
+b = int(input("Введитиь b: "))
+s = a * b
+print("Площа прямокутника зі сторонами а =", a, ", b =", b, "дорівнює", s)
+
+
+
